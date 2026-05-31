dd496f6f176346f80db94f94c1898a1abad16370
Diego-All <dposadallano@gmail.com>
2026-05-31 17:30:59 -0500

    Begin the golang development

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+
 	lambda "github.com/aws/aws-lambda-go/lambda"
 	// go get github.com/aws/aws-lambda-go/events
+	"github.com/aws/aws-lambda-go/events"
 )
 
 func main() {
@@ -11,6 +14,6 @@ func main() {
 
 }
 
-func EjecutoLambda() {
+func EjecutoLambda(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
 
 }
